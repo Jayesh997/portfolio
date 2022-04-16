5752be524056b175462dab812e068e6080939b9e
Gupta <Jayesh.Gupta@igl.co.in>
2022-04-16 16:19:44 +0530

    whatsapp link change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
         {/* <a href='https://www.instagram.com/jayyy_997/' target='_blank'><img className='socialmedia-icons' src={InstagramIcon}/></a> */}
         <a href='https://www.linkedin.com/in/jayesh-gupta-b84619217' target='_blank'><img className='socialmedia-icons' src={LinkedInIcon}/></a>
         <a href='https://github.com/Jayesh997' target='_blank'><img className='socialmedia-icons' src={GithubIcon}/></a>
-        <a href='https://wa.me/9999222523' target='_blank'><img className='socialmedia-icons' src={WhatsappIcon}/></a>
+        <a href='https://wa.me/919999222523' target='_blank'><img className='socialmedia-icons' src={WhatsappIcon}/></a>
         </div>
         <img src={home}/>
         </div>
